16fab1923ddb08d9e247445d8b5807eaf8fa46c9
Juan Camilo Mora <camilo2652@gmaill.com>
2026-08-30 22:51:37 -0500

    Rectangle

---
+++ Backend/Rectangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace GeometricFigures.Backend
+{
+    public class Rectangle : Square
+    {
+        protected double _b;
+
+        public double B
+        {
+            get => _b;
+            set => _b = ValidateB(value);
+        }
+
+        public Rectangle(string name, double a, double b) : base(name, a)
+        {
+            B = b;
+        }
+
+        protected virtual double Validate(double value)
+        {
+            if (value <= 0)
+                throw new ArgumentException("El lado (B) debe ser mayor a cero.");
+            return value;
+        }
+
+        public override double GetArea() => A * B;
+
+        public override double GetPerimeter() => 2 * (A + B);
+        
+
+    }
+}
