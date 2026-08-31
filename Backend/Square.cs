c780523e3939eae53548c20211a99bb4ad2a7413
Juan Camilo Mora <camilo2652@gmaill.com>
2026-08-30 23:25:05 -0500

    Corrections

---
+++ Backend/Square.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GeometricFigures.Backend
+{
+    public class Square : GeometricFigure
+    {
+        protected double _a;
+
+        public double A
+        {
+            get => _a;
+            set => _a = ValidateA(value);
+        }
+
+        public Square(string name, double a)
+        {
+            Name = name;
+            A = a;
+        }
+
+        protected virtual double ValidateA(double value)
+        {
+            if (value <= 0)
+                throw new ArgumentException("El lado (A) debe ser mayor que cero.");
+            return value;
+        }
+
+        public override double GetArea() => Math.Pow(A, 2);
+
+        public override double GetPerimeter() => 4 * A;
+    }
+}
