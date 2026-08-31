f98bb057f5d60b338ae6bde649fe296a2d57e9b5
Juan Camilo Mora <camilo2652@gmaill.com>
2026-08-30 22:41:21 -0500

    Kite

--- Backend/Kite.cs
+++ Backend/Kite.cs
@@ -1,10 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+
 
 namespace GeometricFigures.Backend
 {
-    internal class Kite
+    public class Kite : Rhombus
     {
+        private double _b;
+
+        public double B;
+        {
+            get => _b;
+            set => _b = ValidateB(value);
+        }
+
+        public Kite(string name, double a, double b,  double d1, double d2) : base(name, a, d1, d2)
+        {
+            B = b;
+        }
+
+        private double ValidateB(double value)
+        {
+            if (value <= 0)
+                throw new ArgumentException("El lado (B) debe ser mayor a cero.");
+            return value;
+        }
+
+        public override double GetArea() => (D1 * D2) / 2;
+
+        public override double GetPerimeter() => 2 * (A + B);
+       
+       
     }
 }
