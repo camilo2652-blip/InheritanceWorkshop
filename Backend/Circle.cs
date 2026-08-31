42f02a320db6760255fd2bdf71d9024fc2bc1d6e
Juan Camilo Mora <camilo2652@gmaill.com>
2026-08-30 21:27:33 -0500

    Circle

--- Backend/Circle.cs
+++ Backend/Circle.cs
@@ -2,8 +2,31 @@
 
 namespace GeometricFigures.Backend
 {
-    public class Class1
+    public class Circle : GeometricFigure
     {
+        private double _r;
 
+        public double R;
+        {   get => r;
+            set => _r = ValidateR(value);
+        }
+
+        public Circle(string name, double r)
+        {
+            Name = name;
+            R = r;
+        }
+
+        private double ValidateR(double value)
+        {
+            if (value <= 0)
+                throw new ArgumentException("El radio (R) debe ser mayor a cero.");
+            return value;
+        }
+
+        public override double GetArea() => Math.PI * Math.Pow(R, 2);
+
+        public override double GetPerimeter() => 2 * Math.PI * R;         
+        
     }
 }
