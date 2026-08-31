50f01ffed5b6909f16964e02fe2c17fcc2e9993c
Juan Camilo Mora <camilo2652@gmaill.com>
2026-08-30 22:29:09 -0500

    Rhombus

--- Backend/Rhombus.cs
+++ Backend/Rhombus.cs
@@ -1,10 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+
+
 
 namespace GeometricFigures.Backend
 {
-    internal class Rhombus
+    public class Rhombus : Square
     {
+        public double _d1;
+        public double _d2;
+
+        public double D1
+        {
+            get => _d1;
+            set => _d1 = ValidateD1(value);
+        }
+
+        public double D2
+        {
+            get => _d2;
+            set => _d2 = ValidateD2(value);
+        }
+
+        public Rhombus(string name, double a, double d1, double d2) : base(name, a)        
+        {
+            D1 = d1;
+            D2 = d2;
+        }
+
+        private double ValidateD1(double value)
+        {
+            if (value <= 0)
+                throw new ArgumentException("La diagonal (D1) debe ser mayor a cero.");
+            return value;
+        }
+
+        private double ValidateD2(double value)
+        {
+            if (value <= 0)
+                throw new ArgumentException("La diagonal (D2) debe ser mayor a cero.");
+            return value;
+        }
+
+        public override double GetArea() => (D1 * D2) / 2;
+
+        public override double GetPerimeter() => 4 * A;
+       
+        
     }
 }
